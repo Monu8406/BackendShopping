dc5230cad5b05174b32d1a61f08fa6479f9dd6bb
Monu8406 <124706699+Monu8406@users.noreply.github.com>
2024-03-26 01:50:34 +0530

    ddd

--- payment.js
+++ payment.js
@@ -12,7 +12,7 @@ router.post("/orders", async (req, res) => {
 		});
 
 		const options = {
-			amount: 1000 * 100,
+			amount: req.body.amount * 100,
 			currency: "INR",
 			receipt: crypto.randomBytes(10).toString("hex"),
 		};
@@ -25,14 +25,14 @@ router.post("/orders", async (req, res) => {
 			res.status(200).json({ data: order });
 		});
 	} catch (error) {
-		res.status(500).json({ message: "Internal Server Error!" });
+		res.status(500).json({message: "Internal Server Error!" });
 		console.log(error);
 	}
 });
 
 router.post("/verify", async (req, res) => {
 	try {
-		const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
+		const {razorpay_order_id, razorpay_payment_id, razorpay_signature } =
 			req.body;
 		const sign = razorpay_order_id + "|" + razorpay_payment_id;
 		const expectedSign = crypto
